c95e5ba372bfb6b864268a96c54b9b4496fc39f8
sinsens <xzapk@hotmail.com>
2018-05-14 17:59:23 +0800

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ShutdownOnTime
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbTip = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "时间：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(74, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(253, 29);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(128, 192);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbTip
+            // 
+            this.lbTip.AutoSize = true;
+            this.lbTip.Location = new System.Drawing.Point(107, 161);
+            this.lbTip.Name = "lbTip";
+            this.lbTip.Size = new System.Drawing.Size(65, 12);
+            this.lbTip.TabIndex = 1;
+            this.lbTip.Text = "剩余时间：";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "当前系统时间：";
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(128, 221);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 23);
+            this.btnAbort.TabIndex = 5;
+            this.btnAbort.Text = "终止关机";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(131, 116);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "强制关机";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(63, 30);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(109, 21);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(353, 260);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbTip);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "定时关机";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lbTip;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAbort;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownOnTime
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        bool is_start = false;
+        TimeSpan elapsedSpan;
+        DateTime centuryBegin;
+        DateTime currentDate;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               DateTime dt = Convert.ToDateTime(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("输入的时间格式不正确，应该为\"00:00:00\"格式");
+                return;
+            }
+            
+            var ts = (textBox1.Text.Split(':'));
+            DateTime t = dateTimePicker1.Value;
+            centuryBegin = new DateTime(t.Year, t.Month, t.Day, Convert.ToInt16(ts[0]), Convert.ToInt16(ts[1]), Convert.ToInt16(ts[2]));
+            long elapsedTicks = centuryBegin.Ticks - currentDate.Ticks;
+            if (elapsedTicks/1000 < 60) {
+                MessageBox.Show("定期时间应至少大于当前时间60秒");
+                return;
+            }
+            is_start = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = "当前系统时间："+DateTime.Now.ToLongTimeString();
+            if (is_start)
+            {
+                currentDate = DateTime.Now;
+                long elapsedTicks = centuryBegin.Ticks - currentDate.Ticks;
+                elapsedSpan = new TimeSpan(elapsedTicks);
+
+                if (elapsedSpan.Seconds < 1)
+                {
+                    System.Diagnostics.Process bootProcess = new System.Diagnostics.Process();
+                    bootProcess.StartInfo.FileName = "shutdown";
+                    bootProcess.StartInfo.Arguments = "/s";
+                    if (checkBox1.Checked)
+                        bootProcess.StartInfo.Arguments += " /p /f";
+                    bootProcess.Start();
+                    is_start = false;
+                    lbTip.Text = "时间到，系统将会在1分钟后关机";
+                }
+                else {
+                    lbTip.Text = "剩余时间："+elapsedSpan.Days+"天" + elapsedSpan.Hours + "小时" + elapsedSpan.Minutes + "分" + elapsedSpan.Seconds + "秒";
+                }
+                    
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process bootProcess = new System.Diagnostics.Process();
+            bootProcess.StartInfo.FileName = "shutdown";
+            bootProcess.StartInfo.Arguments = "/a";
+            bootProcess.Start();
+            init();
+            MessageBox.Show("已终止关机");
+        }
+        private void init() {
+            is_start = false;
+            textBox1.Text = DateTime.Now.ToLongTimeString();
+            lbTip.Text = "剩余时间：";
+        }
+    }
+}
